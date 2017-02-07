385446dcd99b2e4fbda1942c56e6b9749290784b
LokiFaun <johannes.schuetz@outlook.com>
2017-02-07 22:45:16 +0100

    Reworked read_rom to use io::Result

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,24 @@
 use std::env;
 use std::fs;
+use std::io;
 use std::io::Read;
 use std::path::Path;
 
 fn main() {
     if let Some(rom_file_name) = env::args().nth(1) {
         println!("ROM: {}", rom_file_name);
-        if let Some(rom) = read_rom(rom_file_name) {
-            println!("{:?}", rom);
-            return;
-        } else {
-            println!("Could not read ROM file!");
-        }
+        match read_rom(rom_file_name) {
+            Ok(rom) => println!("{:?}", rom),
+            Err(err) => println!("Could not read ROM: {}", err),
+        };
     }
 
     println!("Usage: chip8 <rom>");
 }
 
-fn read_rom<P: AsRef<Path>>(path: P) -> Option<Vec<u8>> {
-    match fs::File::open(path) {
-        Ok(mut file) => {
-            let mut file_buffer = Vec::new();
-            match file.read_to_end(&mut file_buffer) {
-                Ok(_) => Some(file_buffer),
-                Err(ex) => {
-                    println!("Could not read from file: {}", ex);
-                    None
-                }
-            }
-        }
-        Err(ex) => {
-            println!("Could not open file: {}", ex);
-            None
-        }
-    }
+fn read_rom<P: AsRef<Path>>(path: P) -> io::Result<Vec<u8>> {
+    let mut file = try!(fs::File::open(path));
+    let mut file_buffer = Vec::new();
+    try!(file.read_to_end(&mut file_buffer));
+    Ok(file_buffer)
 }
